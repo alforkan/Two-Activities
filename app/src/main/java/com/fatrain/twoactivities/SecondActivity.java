f961c14a273d1a81a07f931c4b72f6db884bfd5f
forkan <forkanju44rth@gmail.com>
2020-05-30 19:29:01 +0600

    "Tow Activities"

---
+++ app/src/main/java/com/fatrain/twoactivities/SecondActivity.java
@@ -0,0 +1,40 @@
+package com.fatrain.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.fatrain.android.twoactivites.extra.REPLY";
+    TextView mTextMessage;
+    EditText mReply;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        mTextMessage = findViewById(R.id.text_message);
+        mReply = findViewById(R.id.editText_second);
+        mTextMessage.setText(message);
+        
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString().trim();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+        
+    }
+}
